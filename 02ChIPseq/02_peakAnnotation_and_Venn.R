c385736808284d4fa4157bbceac2d10d84c201c8
Jacob González-Isa <109277147+jacgonisa@users.noreply.github.com>
2023-05-30 18:44:50 +0200

    Update 02_peakAnnotation_and_Venn.R

--- 02ChIPseq/02_peakAnnotation_and_Venn.R
+++ 02ChIPseq/02_peakAnnotation_and_Venn.R
@@ -1,4 +1,31 @@
-### Get genebody.bed and chromosome_lengths.txt
+
+#Installing the annotation for K. nitens
+library(devtools)
+install_github("fran-romero-campero/AlgaeFUN/packages/txdb_packages/TxDb.Knitens.Phycocosm")
+library(TxDb.Knitens.Phycocosm)
+install.packages("./org.Knitens.eg.db", repos = NULL, type = "source")
+library(org.Knitens.eg.db)
+
+txdb <- TxDb.Knitens.Phycocosm
+
+#Read peak file obtained after peak calling
+library(ChIPseeker)
+library(clusterProfiler)
+library(ChIPpeakAnno)
+
+peaks <- readPeakFile(peakfile = "IPvsMock.bed", header=F) 
+
+#Annotate peaks
+peakAnno <- annotatePeak(peak=peaks, tssRegion=c(-1000, 100),
+                         TxDb=txdb, annoDb ="org.Knitens.eg.db", ignoreOverlap = F)
+peakLengths= end(peaks) - start(peaks)+1
+
+#Crea
+hist(peakLengths, breaks = 15, col = "#4981BF", 
+     border = "white", xlab = "Peak size (bp)", 
+     ylab = "Number of peaks", main=NULL)
+### Get genebody.bed 
+
 
 
 ### Venn regulome
