e1e3b543105d1bd0caa21717ff75e6b1ec02fcc0
Jacob González-Isa <109277147+jacgonisa@users.noreply.github.com>
2023-05-30 19:41:05 +0200

    Update 03_BigWig_for_deepTools.sh

--- 02ChIPseq/03_BigWig_for_deepTools.sh
+++ 02ChIPseq/03_BigWig_for_deepTools.sh
@@ -10,8 +10,8 @@ sort -k1,1 -k2,2n IPvsINPUT.bedgraph > IPvsINPUT_sorted.bedgraph #Sort BEDGRAPH
 bedGraphToBigWig IPvsINPUT_sorted.bedgraph chromosome_lengths.txt IPvsINPUT.bw #Convert BEDGRAPH to BIGWIG
 
 #genebody.bed was generated in 02_peakAnnotation_and_Venn.R
-computeMatrix  --scale-regions  -b 3000 -a 3000 -R genebody.bed -S IPvsINPUT.bw -o matrix.gz  --outFileSortedRegions regions_H3K27me3.bed  --regionBodyLength 5000  -o matrix.gz --outFileNameMatrix matrix.tab --outFileSortedRegions matrix_sorted_regions.bed
+computeMatrix  --scale-regions  -b 3000 -a 3000 -R genebody.bed -S IPvsINPUT.bw -o matrix.gz  --regionBodyLength 5000  -o matrix.gz --outFileNameMatrix matrix.tab
 #Generate Figure 11
-plotHeatmap -m ../matrix.gz --outFileName HEATMAP.png --colorMap RdBu  --heatmapWidth 10 --heatmapHeight 10 --plotTitle "H3K27me3 gene body enrichment" --kmeans 3 
+ plotHeatmap -m ../matrix.gz --outFileName HEATMAP.png --colorMap RdBu --kmeans 3 --outFileSortedRegions 3sorted_clusters.txt --heatmapWidth 10 --heatmapHeight 10 --plotTitle "H3K27me3 gene body enrichment" --regionsLabel "C1" "C2" "C3"
 
 # FROM SORTED REGIONS FILE "regions_H3K27me3.bed" the analysis of Cluster 1 and 2 is performed in R in step 04
