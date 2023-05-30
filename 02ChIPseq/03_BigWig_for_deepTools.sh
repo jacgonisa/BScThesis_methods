25eb1b2dc7795df9f1779fd98598e382b9c1c46e
Jacob González-Isa <109277147+jacgonisa@users.noreply.github.com>
2023-05-30 18:39:56 +0200

    Update 03_BigWig_for_deepTools.sh

--- 02ChIPseq/03_BigWig_for_deepTools.sh
+++ 02ChIPseq/03_BigWig_for_deepTools.sh
@@ -1,7 +1,13 @@
+cd results
+# Get chromosomes lengths for analysis
+samtools faidx ../genome/knitens.fa
+cut -f1,2 ../genome/knitens.fa.fai > 
+cp ../genome/chromosome_lengths.txt .
 
-awk '{printf "%s\t%d\t%d\t%2.3f\n" , $1,$2,$3,$5}' IPvsINPUT.bed > IPvsINPUT.bedgraph
-sort -k1,1 -k2,2n IPvsINPUT.bedgraph > IPvsINPUT_sorted.bedgraph
-bedGraphToBigWig IPvsINPUT_sorted.bedgraph myChrom.sizes IPvsINPUT.bw
+bedClip IPvsINPUT.bed chromosome_lengths.txt bedClipped.bed #Filtering peaks out of the chromosomes range
+awk '{printf "%s\t%d\t%d\t%2.3f\n" , $1,$2,$3,$5}' bedClipped.bed > IPvsINPUT.bedgraph # Convert BED to BEDGRAPH
+sort -k1,1 -k2,2n IPvsINPUT.bedgraph > IPvsINPUT_sorted.bedgraph #Sort BEDGRAPH
+bedGraphToBigWig IPvsINPUT_sorted.bedgraph chromosome_lengths.txt IPvsINPUT.bw #Convert BEDGRAPH to BIGWIG
 
 
 computeMatrix  --scale-regions  -b 3000 -a 3000 -R genebody.bed -S IPvsINPUT.bw -o matrix.gz  --outFileSortedRegions regions_H3K27me3.bed  --regionBodyLength 5000  -o matrix.gz --outFileNameMatrix matrix.tab --outFileSortedRegions matrix_sorted_regions.bed
